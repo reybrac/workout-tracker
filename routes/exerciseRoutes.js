c5839c4f831628cad0fd4c32c314343d3f5b5f24
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-04-29 22:21:39 -0700

    Fixed issue with graph not updating

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -2,6 +2,30 @@ const router = require("express").Router();
 const { db } = require("../models/Workout");
 const Workout = require("../models/Workout");
 
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
   Workout.aggregate([
     {
@@ -27,32 +51,9 @@ router.get("/api/workouts/range", (req, res) => {
       },
     },
   ])
-    .sort({ day: 1 })
+    .sort({ day: -1 })
     .limit(7)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.put("/api/workouts/:id", (req, res) => {
-  Workout.findByIdAndUpdate(req.params.id, {
-    $push: {
-      exercises: req.body,
-    },
-  })
+    .sort({ day: 1 })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
